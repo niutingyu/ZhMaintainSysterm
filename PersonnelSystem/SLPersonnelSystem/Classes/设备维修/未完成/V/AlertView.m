eacef81ec4eea32f21223d6a62872b829e3d38fb
it026 <it026@slpcb.net>
2025-08-15 19:10:23 +0800

    添加搜索

--- PersonnelSystem/SLPersonnelSystem/Classes/设备维修/未完成/V/AlertView.m
+++ PersonnelSystem/SLPersonnelSystem/Classes/设备维修/未完成/V/AlertView.m
@@ -9,13 +9,23 @@
 #import "AlertView.h"
 
 #import "AssetConst.h"
+#import "DESearchView.h"
+#import "ToolBar.h"
 
 
-@interface AlertView()<UITableViewDelegate,UITableViewDataSource>
+@interface AlertView()<UITableViewDelegate,UITableViewDataSource>{
+    NSString *_condition;
+}
 
 @property (nonatomic,strong)UITableView *tableView;
 
 @property (nonatomic,strong)NSArray * datasource;
+@property (nonatomic,strong)ToolBar * tool;
+
+@property (nonatomic,strong)NSMutableArray *filterMutableArray;
+
+@property (nonatomic,strong)NSArray * filterArray;
+
 @end
 @implementation AlertView
 
@@ -29,12 +39,16 @@
     if (self = [super init]) {
         [self setupUI];
         self.datasource = datasource;
+        self.filterArray = datasource;
         _maintainIdBlock =maintainId;
     }
     return self;
 }
 
 -(void)setupUI{
+    
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(changeText:) name:UITextFieldTextDidChangeNotification object:nil];
+    
     self.frame = [UIScreen mainScreen].bounds;
     self.backgroundColor = [UIColor colorWithRed:0/255.0 green:0/255.0 blue:0/255.0 alpha:0.3/1.0];
     //bgView最大高度
@@ -72,11 +86,46 @@
     titleLab.textAlignment = NSTextAlignmentCenter;
     titleLab.font = [UIFont systemFontOfSize:17];
     [updateView addSubview:titleLab];
+    
+    DESearchView * searchView = [[NSBundle mainBundle]loadNibNamed:@"DESearchView" owner:self options:nil].firstObject;
+    searchView.frame = CGRectMake(0, 30, updateView.frame.size.width, 48);
+    
+    searchView.searchContentTextField.inputAccessoryView =self.tool;
+    
+    [updateView addSubview:searchView];
+    
+    KWeakSelf
+    searchView.searchBlock = ^{
+       [weakSelf.filterMutableArray removeAllObjects];
+       
+        
+        if (self->_condition.length == 0) {
+            weakSelf.filterArray = weakSelf.datasource;
+            [weakSelf.tableView reloadData];
+            return ;
+        }
+        [weakSelf endEditing:YES];
+        for (NSDictionary * model in weakSelf.datasource) {
+            NSString * name =model[@"MaintainFaultName"];
+            
+      
+           
+            NSRange range = [name rangeOfString:self->_condition options:NSCaseInsensitiveSearch];
+            if (range.location !=NSNotFound) {
+               
+                [weakSelf.filterMutableArray addObject:model];
+                
+            }
+        }
+        weakSelf.filterArray =[weakSelf.filterMutableArray copy];
+        [weakSelf.tableView reloadData];
+       
+    };
     [self showWithAlert:bgView];
     
     //tableview
     if (!_tableView) {
-        _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(titleLab.frame)+6, updateView.frame.size.width, updateView.frame.size.height) style:UITableViewStyleGrouped];
+        _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, CGRectGetMaxY(searchView.frame)+6, updateView.frame.size.width, updateView.frame.size.height) style:UITableViewStyleGrouped];
         _tableView.delegate = self;
         _tableView.dataSource = self;
         _tableView.tableFooterView = [UIView new];
@@ -89,22 +138,22 @@
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return self.datasource.count;
+    return self.filterArray.count;
 }
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"cellId"];
     cell.textLabel.font = [UIFont systemFontOfSize:15];
     cell.selectionStyle = UITableViewCellEditingStyleNone;
-    cell.textLabel.text = self.datasource[indexPath.row][@"MaintainFaultName"];
+    cell.textLabel.text = self.filterArray[indexPath.row][@"MaintainFaultName"];
     
     return cell;
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
 
-    NSString * maintainId = self.datasource[indexPath.row][@"MaintainFaultId"];//id
-    NSString * name = self.datasource[indexPath.row][@"MaintainFaultName"];
+    NSString * maintainId = self.filterArray[indexPath.row][@"MaintainFaultId"];//id
+    NSString * name = self.filterArray[indexPath.row][@"MaintainFaultName"];
     if (self.maintainIdBlock) {
         self.maintainIdBlock(maintainId, name);
     }
@@ -139,7 +188,34 @@
     
 }
 
+-(void)changeText:(NSNotification*)notification{
+    UITextField * textField = [notification object];
+    _condition = textField.text;
+    
+}
+
 -(void)cancel{
     [self dismissAlert];
 }
+
+- (ToolBar *)tool{
+    if (!_tool) {
+        _tool = [ToolBar toolBar];
+        
+        __weak typeof(self) weakself = self;
+        _tool.finishBlock = ^(){
+            [weakself endEditing:YES];
+        };
+    }
+    return _tool;
+}
+
+-(NSMutableArray*)filterMutableArray{
+    if (!_filterMutableArray) {
+        _filterMutableArray  =[NSMutableArray array];
+    }return _filterMutableArray;
+}
+
+
+
 @end
